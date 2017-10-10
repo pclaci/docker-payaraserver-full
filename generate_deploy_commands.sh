bbcec5af2b8185c5aa22a947158fa86d20a60d82
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2017-10-10 15:38:01 +0200

    Fixed variable reference Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

--- generate_deploy_commands.sh
+++ generate_deploy_commands.sh
@@ -1,8 +1,8 @@
 ################################################################################
 #
-# A script to generate the $DEPLOY_COMMANDS file with asadmin commands to deploy 
+# A script to generate the $POSTBOOT_COMMANDS file with asadmin commands to deploy 
 # all applications in $DEPLOY_DIR (either files or folders). 
-# The $DEPLOY_COMMANDS file can then be used with the start-domain using the
+# The $POSTBOOT_COMMANDS file can then be used with the start-domain using the
 #  --postbootcommandfile parameter to deploy applications on startup.
 #
 # Usage:
@@ -23,8 +23,8 @@ if [ x$1 != x ]
     DEPLOY_OPTS="$*"
 fi
 
-echo '# deployments after boot' > $DEPLOY_COMMANDS
+echo '# deployments after boot' > $POSTBOOT_COMMANDS
 for deployment in "${DEPLOY_DIR}"/*
   do
-    echo "deploy --force --enabled=true $DEPLOY_OPTS $deployment" >> $DEPLOY_COMMANDS
+    echo "deploy --force --enabled=true $DEPLOY_OPTS $deployment" >> $POSTBOOT_COMMANDS
 done
