b04c6cb905fec7d242bb090931674e0067994e69
Oliver Bertuch <o.bertuch@fz-juelich.de>
2018-11-22 13:30:09 +0100

    Add support for additional init scripts

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-${SCRIPT_DIR}/generate_deploy_commands.sh
+# Run init scripts (credits go to MySQL Docker entrypoint script)
+for f in ${SCRIPT_DIR}/init_*; do
+      case "$f" in
+        *.sh)  echo "[Entrypoint] running $f"; . "$f" ;;
+        *)     echo "[Entrypoint] ignoring $f" ;;
+      esac
+      echo
+done
+
 exec ${SCRIPT_DIR}/startInForeground.sh
